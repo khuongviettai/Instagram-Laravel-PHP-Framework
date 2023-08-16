bf9966c6ab0ea15ae753356133ed458c66b2342c
Khương Viết Tài <khuongtai@MacBook-Pro.local>
2023-08-16 23:23:44 +0700

    update

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -7,16 +7,19 @@
             <img src="/svg/logo.svg" alt="logo" class="rounded-circle" style="height: 120px; width: 120px">
         </div>
         <div class="col-9 pt-5">
-            <div><h1>KhuongVietTai</h1></div>
+            <div class="d-flex justify-content-between align-items-baseline">
+                <h1>{{ $user->username }}</h1>
+                <a href="">Add New Post</a>
+            </div>
             <div class="d-flex">
                 <div class="pe-5"><strong>153</strong> post</div>
                 <div class="pe-5"><strong>103K</strong> follower</div>
                 <div class="pe-5"><strong>200</strong> flowing</div>
             </div>
-            <div class="pt-4" style="font-weight: bold">khuongviettai.com</div>
-            <div>Khuongviettai.com is a place to exchange and learn about finance</div>
+            <div class="pt-4" style="font-weight: bold">{{$user->profile->title}}</div>
+            <div>{{$user->profile->description}}</div>
             <div>
-                <a href="#" style="text-decoration: none">www.khuongviettai.com</a>
+                <a href="#" style="text-decoration: none">{{$user->profile->url}}</a>
             </div>
         </div>
     </div>
